a3073302dd2970ecbd2b0c5b1bcd9fbea232dd04
Nagastuma <40513995+NagatsumaHiroki@users.noreply.github.com>
2021-07-11 16:04:30 +0900

    React_todo 初回コミット ・基本構造コミット

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,40 @@
 import React from "react";
-import Colorfull from "./components/Colorfull";
+import "./styles.css";
 
-const App = () => {
-  const onClickButton = () => alert();
-  const contetStyle = {
-    color: "blue",
-    fontSize: "18px"
-  };
-  const contetLedyStyle = {
-    color: "pink",
-    fontSize: "18px"
-  };
+export const App = () => {
   return (
-    <React.Fragment>
-      <h1 style={{ color: "red" }}>こんにちは!</h1>
-      <Colorfull color="blue" message="お元気ですか" />
-      <p style={contetLedyStyle}>元気です</p>
-      <button onClick={onClickButton}>ボタン</button>
-    </React.Fragment>
+    <>
+      <div>
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div>
+        <p>未完了</p>
+        <ul>
+          <div>
+            <li>aaaaa</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+          <div>
+            <li>aaaaa</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+        </ul>
+      </div>
+      <div>
+        <p>完了のTODO</p>
+        <ul>
+          <div>
+            <li>aaaaa</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+        </ul>
+      </div>
+      <div></div>
+    </>
   );
 };
 
