db2e3fd31c256ae282d4d2366548714f76936dd8
Nagastuma <40513995+NagatsumaHiroki@users.noreply.github.com>
2021-07-16 00:25:27 +0900

    追加機能作成 作成日：2021_07_16 追加機能作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./styles.css";
 
 export const App = () => {
+  const [todoText, setTodotext] = useState("");
   const [icompleteTodos, setIncompleteTodos] = useState([
     "あああああ",
     "いいいい"
   ]);
+
+  const [completeTodos, setCompleteTodos] = useState(["うううう"]);
+
+  //入力時にsetTodotextに値を設定
+  const onChangeTodoText = (event) => setTodotext(event.target.value);
+
+  const onClickAdd = () => {
+    const newTodos = [...icompleteTodos, todoText];
+    setIncompleteTodos(newTodos);
+    alert(todoText);
+  };
+
   return (
     <>
       <div>
-        <input placeholder="TODOを入力" />
-        <button>追加</button>
+        <input
+          placeholder="TODOを入力"
+          value={todoText}
+          onChange={onChangeTodoText}
+        />
+        <button onClick={onClickAdd}>追加</button>
       </div>
       <div>
         <p>未完了</p>
@@ -29,11 +46,14 @@ export const App = () => {
       <div>
         <p>完了のTODO</p>
         <ul>
-          <div>
-            <li>aaaaa</li>
-            <button>完了</button>
-            <button>削除</button>
-          </div>
+          {completeTodos.map((todo) => {
+            return (
+              <div key={todo} classNmae="list-row">
+                <li>{todo}</li>
+                <button>戻す</button>
+              </div>
+            );
+          })}
         </ul>
       </div>
       <div></div>
