6775bb8d0a29c68a29f47a1fb872512196b2cb32
Nagastuma <40513995+NagatsumaHiroki@users.noreply.github.com>
2021-07-18 00:36:40 +0900

    削除、追加機能作成

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,37 @@ import "./styles.css";
 
 export const App = () => {
   const [todoText, setTodotext] = useState("");
-  const [icompleteTodos, setIncompleteTodos] = useState([
-    "あああああ",
-    "いいいい"
-  ]);
+  const [icompleteTodos, setIncompleteTodos] = useState([""]);
 
-  const [completeTodos, setCompleteTodos] = useState(["うううう"]);
+  const [completeTodos, setCompleteTodos] = useState([""]);
 
   //入力時にsetTodotextに値を設定
   const onChangeTodoText = (event) => setTodotext(event.target.value);
 
+  //登録処理
   const onClickAdd = () => {
+    if (todoText === "") return;
     const newTodos = [...icompleteTodos, todoText];
     setIncompleteTodos(newTodos);
     alert(todoText);
+    setTodotext("");
+  };
+
+  //削除処理
+  const onClickDelete = (index) => {
+    const newTodos = [...icompleteTodos];
+    newTodos.splice(index, 1);
+    setIncompleteTodos(newTodos);
+  };
+
+  //完了処理
+  const onClickComplete = (index) => {
+    const newIncompleteTodos = [...icompleteTodos];
+    newIncompleteTodos.splice(index);
+
+    const newCompleteTodos = [...completeTodos, icompleteTodos[index]];
+    setIncompleteTodos(newIncompleteTodos);
+    setCompleteTodos(newCompleteTodos);
   };
 
   return (
@@ -32,12 +49,15 @@ export const App = () => {
       <div>
         <p>未完了</p>
         <ul>
-          {icompleteTodos.map((todo) => {
+          {icompleteTodos.map((todo, index) => {
+            if (todo === "") {
+              return;
+            }
             return (
               <div key={todo} class="list-row">
-                <li>aaaaa</li>
-                <button>完了</button>
-                <button>削除</button>
+                <li>{todo}</li>
+                <button onClick={() => onClickComplete(index)}>完了</button>
+                <button onClick={() => onClickDelete(index)}>削除</button>
               </div>
             );
           })}
@@ -47,6 +67,9 @@ export const App = () => {
         <p>完了のTODO</p>
         <ul>
           {completeTodos.map((todo) => {
+            if (todo === "") {
+              return;
+            }
             return (
               <div key={todo} classNmae="list-row">
                 <li>{todo}</li>
